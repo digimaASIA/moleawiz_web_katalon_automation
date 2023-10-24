7ed8ef0551fe330f856b5b82a7f2daba4d927da0
Hafizh Abdillah <Hafizh Abdillah@DESKTOP-JE6N759>
2023-10-24 18:39:10 +0700

    update scenario swipe and scroll in menu home

---
+++ Object Repository/Object Home/btn_Continue_Ongoing_Course_Index_5_Home.rs
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_Continue_Ongoing_Course_Index_5_Home</name>
+   <tag></tag>
+   <elementGuidId>96d4bddc-f181-465f-8b65-1de3552f5c99</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@name = 'btn-continue-ongoing-course-home' and @data-index = '2']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>.swiper-backface-hidden > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>.swiper-backface-hidden > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>btn-continue-ongoing-course-home</value>
+      <webElementGuid>324d81e4-d34c-453b-bfde-373c847b6bd6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-index</name>
+      <type>Main</type>
+      <value>2</value>
+      <webElementGuid>cad4dfce-592e-47b0-9021-46bbd7f8e619</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
--- Test Suites/MoLeaWiz/Home/Positive Case/Scenario Positive Case - Home.ts
+++ Test Suites/MoLeaWiz/Home/Positive Case/Scenario Positive Case - Home.ts
@@ -12,136 +12,24 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>073a3426-a97c-48c2-920c-c94ab80c893c</testSuiteGuid>
    <testCaseLink>
-      <guid>2eef4059-5a87-43e8-9e9e-8f3b2ff74b54</guid>
+      <guid>40b1c44a-c7aa-44a9-91c0-0e283c3924f4</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH01 - User Open My Learning Journey</testCaseId>
+      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH20 - User can Swipe Banner when click button Carousel in Home Page</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>52a98126-acc1-43dc-b4a8-07d08bb171ee</guid>
+      <guid>3f9938c7-254e-4708-8aa8-d4ed583cd167</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH02 - User Open Achievements</testCaseId>
+      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH22 - User can scroll list data Expired in Month in Home Page</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>44e9b21b-b25c-478c-8b85-4a278fef1eb6</guid>
+      <guid>0db52150-dde7-4560-bed1-aaab86cbf432</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH03 - User Open Rewards</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>738c0b9a-8f69-405e-ad40-9e80f51486b6</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH04 - User Open Leaderboards</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>701b7eca-8fd7-41c4-85e5-e46a9c917e16</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH05 - User Open Team Monitoring</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>6eb0ad57-0389-46b8-85c0-6d58a47501f7</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH06 - User Open Reviews</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>e75bdb45-2eae-415d-a078-bf6b7a2065db</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH07 - User Open Help</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>ccc106b2-82b3-4759-a1a9-a622c2935cf5</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH08 - User Open Home</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>ee6f368c-8847-422b-851d-d5331c480d1c</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH09 - User can Swipe Next and Previous Banner in Home Page</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>2952f993-496f-4808-9e56-bbfe172ddb5e</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH10 - User can Swipe Next and Previous New Program in Home Page</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>04db588b-8a98-4232-9703-725faf2969f8</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH11 - User can Swipe Next and Previous Ongoing Course in Home Page</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>07d6aa88-ad23-4dc5-9526-eb8907eca5ee</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH12 - User can Open My Learning Journey while Collapse Expand</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>bccbb1f3-a1dc-4b45-aa50-f1256888c93d</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH13 - User can Open Achievements while Collapse Expand</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>1ac61b44-8214-4eea-b6b3-f946c60cee4a</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH14 - User can Open Rewards while Collapse Expand</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>5d732e82-3fae-4c2e-bc62-eeca3fc67dfa</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH15 - User can Open Leaderboards while Collapse Expand</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>f19700eb-6395-43f4-bb73-76f89ee302ce</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH16 - User can Open Team Monitoring while Collapse Expand</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>e1b3d11f-fccf-467c-80b8-6129d5c5a40a</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH17 - User can Open Reviews while Collapse Expand</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>1f307abe-0e81-499d-9964-4b8e51930e1c</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH18 - User can Open Help while Collapse Expand</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>c57b0381-7ed5-4ef0-b164-099f3346844b</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH19 - User can Open Home while Collapse Expand</testCaseId>
+      <testCaseId>Test Cases/MoLeaWiz/Home/Positive Case/TCMH23 - User can scroll list data Ongoing Program in Month in Home Page</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
