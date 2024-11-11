a9c85bbcb1fea77faae148e275735e8640074d7c
Digimasia <hafizh@digimasia.com>
2024-11-11 16:47:38 +0700

    [UPDATE] update step negative case login TCML08

--- Scripts/MoLeaWiz/Login/Negative Case/TCML08 - User tidak bisa login karena terlalu banyak melakukan percobaan login dengan invalid username dan password/Script1699955840020.groovy
+++ Scripts/MoLeaWiz/Login/Negative Case/TCML08 - User tidak bisa login karena terlalu banyak melakukan percobaan login dengan invalid username dan password/Script1699955840020.groovy
@@ -43,12 +43,6 @@ WebUI.click(findTestObject('Object Login/btn_login'))
 
 WebUI.delay(2)
 
-CustomKeywords.'keyword_FunctionLogin.CapchaLogin.automationCaptcha'()
-
-WebUI.click(findTestObject('Object Login/btn_login'))
-
-WebUI.delay(2)
-
 WebUI.verifyElementVisible(findTestObject('Object Login/verify_Unsuccessfully_Login - Too many failed login attempts Please try again in 1 minutes'), 
     FailureHandling.STOP_ON_FAILURE)
 
